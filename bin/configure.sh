7d2c9542a9e835ce401fd545ab0e9da8e62b1a39
tlancaster6 <tuckerlancaster@gmail.com>
2022-06-27 15:05:19 -0400

    continuing to refine config script

--- bin/configure.sh
+++ bin/configure.sh
@@ -6,6 +6,7 @@ git reset --hard HEAD
 git pull
 # install dependencies
 conda create -n IOFtrain python=3.7
+conda activate IOFtrain
 conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
