dacc7fc98197b3c6b2fed3788bf060303ef5682e
tlancaster6 <tuckerlancaster@gmail.com>
2023-03-06 14:05:14 -0500

    automating cleanup of checkpoint files

--- bin/configure.sh
+++ bin/configure.sh
@@ -8,6 +8,7 @@ git pull
 conda create -n IOFtrain python=3.9
 conda activate IOFtrain
 conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
+conda install -c conda-forge wandb
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 #conda install -c conda-forge imgaug
